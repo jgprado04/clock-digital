4f74fd3b4fe9bc1df9f2fde8d44a77fe46ac45d0
JG <jgprado04@gmail.com>
2023-06-26 11:26:20 -0300

    Style ajeitado

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function setTime() {
-    let h1 = document.querySelector("h1");
-    let date = new Date();
 
+function setTime() {
+    let date = new Date()
+    let h1 = document.querySelector("h1");
     h1.textContent = date.toLocaleTimeString();
+
+    let p1 = document.querySelector("p1");
+    p1.textContent = date.toDateString();
 }
 
 setInterval(setTime, 1000);
+
